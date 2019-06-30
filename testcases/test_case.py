de87365d3913c3930d0fdd89e8c17d35a2e8a316
limeng201901 <1587517125@qq.com>
2019-06-30 17:28:53 +0800

    cc

--- testcases/test_case.py
+++ testcases/test_case.py
@@ -4,9 +4,9 @@ import allure
 from selenium import webdriver
 import time
 
-@allure.feature("好爸爸")
+@allure.feature("好爸爸hhhhhhhh")
 class Testjenkins:
  def test_jenkins_pytest(self):
-   with allure.step("测试"):
+   with allure.step("测试hhhhhhhhhhh"):
     driver = webdriver.Chrome()
     driver.get("http://www.baidu.com")
